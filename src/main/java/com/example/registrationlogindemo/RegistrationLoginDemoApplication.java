9c90ba0e35c9ef3ba162f47920874f364934e9b3
fedegonc <goncalvezfede@gmail.com>
2024-04-23 19:12:13 -0300

    ajuste main

--- src/main/java/com/example/registrationlogindemo/RegistrationLoginDemoApplication.java
+++ src/main/java/com/example/registrationlogindemo/RegistrationLoginDemoApplication.java
@@ -7,9 +7,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RegistrationLoginDemoApplication {
 
 	public static void main(String[] args) {
-		// @SpringBootApplication: Esta anotación combina tres anotaciones: @Configuration, @EnableAutoConfiguration y @ComponentScan. Marca esta clase como una clase de configuración de Spring, habilita la configuración automática de Spring Boot y escanea los paquetes para encontrar componentes y configuración.
-		// pasando la clase principal RegistrationLoginDemoApplication y los argumentos de línea de
-		// comandos como parámetros.
+		// @SpringBootApplication: Esta anotación combina tres anotaciones:
+		// @Configuration, @EnableAutoConfiguration y @ComponentScan.
+		// Marca esta clase como una clase de configuración de Spring,
+		// habilita la configuración automática de Spring Boot y escanea
+		// los paquetes para encontrar componentes y configuración.
+		// pasando la clase principal RegistrationLoginDemoApplication y los argumentos
+		// de línea de comandos como parámetros.
+
 		SpringApplication.run(RegistrationLoginDemoApplication.class, args);
 	}
 }
