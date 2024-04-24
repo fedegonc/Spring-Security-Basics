b21c380911357a006ebad6500e96b5b9df62d29c
fedegonc <goncalvezfede@gmail.com>
2024-04-23 21:56:16 -0300

    ajuste css index

--- src/main/java/com/example/registrationlogindemo/RegistrationLoginDemoApplication.java
+++ src/main/java/com/example/registrationlogindemo/RegistrationLoginDemoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RegistrationLoginDemoApplication {
+public class 	RegistrationLoginDemoApplication {
 
 	public static void main(String[] args) {
 		// @SpringBootApplication: Esta anotación combina tres anotaciones:
