afe6d15ef09f2fa5dc8b48caa05ee4dec09d5972
federico goncalvez <97002761+fedegonc@users.noreply.github.com>
2024-04-30 17:29:20 -0300

    ajuste index

---
+++ src/main/java/com/example/registrationlogindemo/FronteraSustentableApplication.java
@@ -0,0 +1,12 @@
+package com.example.registrationlogindemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FronteraSustentableApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FronteraSustentableApplication.class, args);
+	}
+}
+//private-policy
