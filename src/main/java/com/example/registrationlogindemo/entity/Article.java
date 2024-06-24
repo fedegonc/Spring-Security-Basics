7e94c82167350d580a8b5b2c7bdf7e313899fa06
federico goncalvez <97002761+fedegonc@users.noreply.github.com>
2024-06-24 10:10:41 -0300

    add article link template campos

--- src/main/java/com/example/registrationlogindemo/entity/Article.java
+++ src/main/java/com/example/registrationlogindemo/entity/Article.java
@@ -22,76 +22,19 @@ public class Article {
     private int id;
 
     @NotNull
-    private String nombre;
-
-    @NotNull
-    private String categoria;
-
-    @NotNull
-    private boolean activo = true; // Inicializar con true por defecto
+    private String titulo;
 
     @Lob
     private String descripcion = "";
 
     private String imagen;
 
-    @NotNull
-    private String diasDisponibles;
-
-    @NotNull
-    private String horaRecoleccion;
-
     @Transient
     private MultipartFile file;
 
-    @NotNull
-    private LocalDateTime fecha;
-
-    @NotNull
-    @Enumerated(EnumType.STRING)
-    private Solicitude.Estado estado = Solicitude.Estado.EN_ESPERA;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-
-    @NotNull
-    private String calle;
-
-    @NotNull
-    private String barrio;
-
-    @NotNull
-    private String numeroDeCasa;
-
-    @NotNull
-    private String telefono;
-
-    @NotNull
-    private String peso;
-
-    @NotNull
-    private String volumen;
-
-    @NotNull
-    private String destino;
-
-    public enum Estado {
-        EN_ESPERA,
-        ACEPTADA,
-        RECHAZADA,
-        EN_REVISION
-    }
-
-    @PrePersist
-    @PreUpdate
-    private void prePersistUpdate() {
-        if (Objects.isNull(this.descripcion)) {
-            this.descripcion = "";
-        }
-        if (Objects.isNull(this.fecha)) {
-            this.fecha = LocalDateTime.now();
-        }
-    }
 }
