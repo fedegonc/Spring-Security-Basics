cf8f5cbccd1712bcc19e872addb1ef62f2fae112
federico goncalvez <97002761+fedegonc@users.noreply.github.com>
2024-09-24 16:00:51 -0300

    fix index fragments

--- src/main/resources/static/scripts/guestScript.js
+++ src/main/resources/static/scripts/guestScript.js
@@ -1,28 +1,13 @@
-function openNav() {
-    document.getElementById("mySidenav").style.left = "0";
-    document.getElementById("sidenavToggleNav").style.left = "250px";
-}
 
-function closeNav() {
-    document.getElementById("mySidenav").style.left = "-250px";
-    document.getElementById("sidenavToggleNav").style.left = "0";
-}
+window.addEventListener("scroll", function() {
+    const navbar = document.getElementById("mainNavbar");
+    const logo = document.getElementById("navbarLogo");
 
-document.getElementById("sidenavToggleNav").addEventListener("click", function () {
-    const sidenav = document.getElementById("mySidenav");
-    if (sidenav.style.left === "0px") {
-        closeNav();
-    } else {
-        openNav();
-    }
-});
-window.addEventListener('scroll', function() {
-    var navbar = document.getElementById("mainNavbar");
-
-    // Si la página ha hecho scroll más de 50px, añadir la clase 'scrolled'
-    if (window.scrollY > 50) {
+    if (window.scrollY > 50) { // Cambia el valor según tu preferencia
         navbar.classList.add("scrolled");
+        logo.style.width = "40px"; // Cambia el tamaño del logo al hacer scroll
     } else {
         navbar.classList.remove("scrolled");
+        logo.style.width = "50px"; // Vuelve al tamaño original
     }
 });
