b918dff412fe3ad27afd7e8119244639721c0ff9
federico goncalvez <97002761+fedegonc@users.noreply.github.com>
2024-10-01 18:33:41 -0300

    style: desacoplando css

--- src/main/resources/static/scripts/userScript.js
+++ src/main/resources/static/scripts/userScript.js
@@ -34,23 +34,3 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 });
-
-function openNav() {
-    document.getElementById("mySidenav").style.left = "0";
-    document.getElementById("mySidenav").classList.add('open'); // Añade clase 'open'
-}
-
-function closeNav() {
-    document.getElementById("mySidenav").style.left = "-250px";
-    document.getElementById("mySidenav").classList.remove('open'); // Quita clase 'open'
-}
-
-// Event listener para el botón
-document.getElementById("sidenavToggleNav").addEventListener("click", function () {
-    const sidenav = document.getElementById("mySidenav");
-    if (sidenav.classList.contains('open')) {
-        closeNav();
-    } else {
-        openNav();
-    }
-});
