b0b649672d3abe3b9570b01ad175b492ffdbfd3d
federico goncalvez <97002761+fedegonc@users.noreply.github.com>
2024-10-01 21:29:38 -0300

    style: ajustes logo navbrand

--- src/main/resources/static/scripts/adminScript.js
+++ src/main/resources/static/scripts/adminScript.js
@@ -1,22 +1,3 @@
-function openNav() {
-    document.getElementById("mySidenav").style.left = "0";
-    document.getElementById("mySidenav").classList.add('open'); // Añade clase 'open'
-}
-
-function closeNav() {
-    document.getElementById("mySidenav").style.left = "-250px";
-    document.getElementById("mySidenav").classList.remove('open'); // Quita clase 'open'
-}
-
-// Event listener para el botón
-document.getElementById("sidenavToggleNav").addEventListener("click", function () {
-    const sidenav = document.getElementById("mySidenav");
-    if (sidenav.classList.contains('open')) {
-        closeNav();
-    } else {
-        openNav();
-    }
-});
 
 // Gráfico de Barras para Usuarios Activos
 const ctxUsers = document.getElementById('usersChart').getContext('2d');
